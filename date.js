de1591854dbcc02b99fc87e306d3bd9b3e71f5f9
Eijah Duane Prince <Prince@Eijahs-MacBook-Pro.local>
2015-10-19 17:08:27 -0500

    I think I did it

---
+++ date.js
@@ -0,0 +1,70 @@
+$ (document).ready(function(){
+  function timedisplay(){
+  	var displaytime = new Date();
+  	var hours = displaytime.getHours();
+  	var minutes = displaytime.getMinutes();
+  	var seconds = displaytime.getSeconds();
+  	var meridiem = "AM";
+  	if(seconds < 10 ){
+  		seconds = "0" + seconds;
+  	}
+
+	if(minutes < 10 ){
+  		minutes = "0" + minutes;
+  	}
+
+
+  	if(hours > 12 ){
+  		hours = hours - 12;
+  		meridiem = "PM";
+
+  	}
+
+  	if (hours === 0){
+  		hours = 12;
+  	}
+
+
+  	var Timer = document.getElementById("Timer");
+Timer.innerHTML = hours + ":" + minutes + ":" + seconds + " ";
+
+  }
+
+
+timedisplay();
+setInterval(timedisplay,1000)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
