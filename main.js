1a8e8cf8869a7de008e16d9c94baddc56d155764
Jaimarie Velasco <jaimariev@Jaimaries-MacBook-Pro.local>
2015-10-19 20:04:49 -0700

    finished normal

--- main.js
+++ main.js
@@ -16,3 +16,17 @@ function getTime(){
 	setInterval(getTime, 1000);
 });
 
+function getTime(){
+	var displayTime = new Date();
+	var hrs = displayTime.getHours();
+	var mins = displayTime.getMinutes();
+	var secs = displayTime.getSeconds();
+		mins = (mins < 10 ? "0" : "") + mins;
+		secs = (secs < 10 ? "0" : "") + secs;
+		hrs = (hrs < 10 ? "0" : "") + hrs;
+	var time = document.getElementById("hard");
+	time.innerHTML = "#" + hrs + mins + secs;
+
+}
+	getTime();
+	setInterval(getTime, 1000);
