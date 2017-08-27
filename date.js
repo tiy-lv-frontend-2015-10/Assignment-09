c6ec4fcc544ef0807b80bf0d85f64585ff64ae54
Elijah Prince <elijahprince73@gmail.com>
2017-08-27 13:33:04 -0700

    Timer

--- date.js
+++ date.js
@@ -1,75 +1,16 @@
-$ (document).ready(function(){
-  function timedisplay(){
-  	var displaytime = new Date();
-  	var hours = displaytime.getHours();
-  	var minutes = displaytime.getMinutes();
-  	var seconds = displaytime.getSeconds();
-  	
-  	if(seconds < 10 ){
-  		seconds = "0" + seconds;
-  	}
+function startTime() {
+  var date = new Date();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+  var t = setTimeout(startTime, 500)
 
-	if(minutes < 10 ){
-  		minutes = "0" + minutes;
-  	}
-
-	
-  	if(hours > 12 ){
-  		hours = "0" +  hours - 12 ;
-  		
-
-  	}
-
-
-
-  	if (hours === 0){
-  		hours = 12;
-  	}
-
-  	
-
-
-
-  	var Timer = document.getElementById("Timer");
-Timer.innerHTML = hours + ":" + minutes + ":" + seconds + " ";
-
+  if (hours >= 12) {
+    hours = hours - 12
   }
 
-
-timedisplay();
-setInterval(timedisplay,1000)
-});
+  document.getElementById('timer').innerHTML = hours + ":" + minutes + ":" + seconds;
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+startTime()
